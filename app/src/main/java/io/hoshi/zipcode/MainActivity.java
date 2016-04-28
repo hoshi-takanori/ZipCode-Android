48b1d81a49eac759b2cfc5e7100a95789914b21c
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2016-04-28 18:19:39 +0900

    add start button

--- app/src/main/java/io/hoshi/zipcode/MainActivity.java
+++ app/src/main/java/io/hoshi/zipcode/MainActivity.java
@@ -2,12 +2,27 @@ package io.hoshi.zipcode;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View startButton;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.start);
+        textView = (TextView) findViewById(R.id.textview);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hello, World!");
+            }
+        });
     }
 }
